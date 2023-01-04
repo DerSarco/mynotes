4439d54a12c54ee600fb39f8a02855bb536395da
Carlos Muñoz Bustamante <carlos.a.munoz2@outlook.com>
2023-01-04 13:09:16 -0300

    added verify email view, added named route navigation, added email verification

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -94,6 +94,14 @@ class _RegisterViewState extends State<RegisterView> {
                     },
                     child: const Text('Register'),
                   ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                          "/login/",
+                              (route) => false,
+                        );
+                      },
+                      child: const Text("Already registered? Login here!"))
                 ],
               );
             default:
