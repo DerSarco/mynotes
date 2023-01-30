9c284f0edf0d2130994f4ef2ee2c2372d5ee8be8
Carlos Muñoz Bustamante <carlos.a.munoz2@outlook.com>
2023-01-30 10:43:26 -0300

    constants routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
@@ -12,9 +13,9 @@ void main() {
     debugShowCheckedModeBanner: false,
     title: 'Flutter Demo',
     routes: {
-      '/login/': (context) => const LoginView(),
-      '/register/': (context) => const RegisterView(),
-      '/notes/': (context) => const NotesView(),
+      loginRoute: (context) => const LoginView(),
+      registerRoute: (context) => const RegisterView(),
+      notesRoute: (context) => const NotesView(),
     },
     theme: ThemeData(
       primarySwatch: Colors.blue,
@@ -93,7 +94,7 @@ class _NotesViewState extends State<NotesView> {
 
   void onButtonTapped(BuildContext context) {
     Navigator.of(context).pushNamedAndRemoveUntil(
-      '/login/',
+      loginRoute,
       (route) => false,
     );
   }
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
+import 'package:mynotes/constants/routes.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
 
@@ -71,7 +73,7 @@ class _LoginViewState extends State<LoginView> {
                     email: email, password: password);
                 if (!mounted) return;
                 Navigator.of(context).pushNamedAndRemoveUntil(
-                  '/notes/',
+                  notesRoute,
                   (route) => false,
                 );
               } on FirebaseAuthException catch (e) {
@@ -90,7 +92,7 @@ class _LoginViewState extends State<LoginView> {
           TextButton(
             onPressed: () {
               Navigator.of(context).pushNamedAndRemoveUntil(
-                "/register/",
+                registerRoute,
                 (route) => false,
               );
             },
